f638191efbc0bd64c1fe06c6634da1547ddfa14c
TuRtLesz <turtleszac@gmail.com>
2023-06-04 11:37:09 +0530

    player_moment+delta_time+screen_zoom_out

--- tha DOG.py
+++ tha DOG.py
@@ -1,11 +1,50 @@
-import pygame,sys
+import pygame,sys,time,random
 display_window=pygame.display.set_mode((800,450))
+game_window=pygame.Surface((1600,900))
 pygame.display.set_caption('tha_DOG')
 clock=pygame.time.Clock()
 game_mode='in_game'
 game_settings={'fullscreen':False}
+
+class player(pygame.sprite.Sprite):
+    def __init__(player):
+        super().__init__()
+        player.image=pygame.image.load('temp.png').convert()
+        player.rect=player.image.get_rect()
+        player.pos=pygame.math.Vector2(player.rect.center)
+        player.velocity=pygame.math.Vector2(0,0)
+        player.acceleration=pygame.math.Vector2(0,0)
+        player.max_velocity=70
+        player.state='idle'
+    def update(player,delta_time):
+        if player.state=='run_right':
+            player.acceleration.x=20
+        if player.state=='run_left':
+            player.acceleration.x=-20
+        if player.velocity.x>player.max_velocity:
+            player.velocity.x=player.max_velocity
+        if player.velocity.x<-player.max_velocity:
+            player.velocity.x=-(player.max_velocity)
+        player.velocity+=player.acceleration*delta_time
+        player.pos+=player.velocity*delta_time
+        player.rect.center=player.pos
+
+player_sprite_group=pygame.sprite.Group()
+dog_sprite_group=pygame.sprite.Group()
+big_fat_guy_sprite_group=pygame.sprite.Group()
+block_sprite_group=pygame.sprite.Group()
+prevoius_time=time.perf_counter()
+
+player_sprite_group.add(player())
 while game_mode=='in_game':
-    display_window.fill((255,255,255))
+    delta_time=time.perf_counter()-prevoius_time
+    prevoius_time=time.perf_counter()
+    game_window.fill((255,255,255))
+    player_sprite_group.update(delta_time)
+    player_sprite_group.draw(game_window)
+    game_window=pygame.transform.smoothscale_by(game_window,0.5)
+    display_window.blit(game_window,(0,0))#screen shake here?
+    keys_pressed=pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
@@ -22,5 +61,20 @@ while game_mode=='in_game':
                 if game_settings['fullscreen']==True:
                     display_window=pygame.display.set_mode((800,450))
                     game_settings['fullscreen']=False
+    for player in player_sprite_group:
+        if keys_pressed[pygame.K_d]:
+            player.state='run_right'
+        if keys_pressed[pygame.K_a]:
+            player.state='run_left'
+        if keys_pressed[pygame.K_w]:
+            player.state='jump'
+            if keys_pressed[pygame.K_d]:
+                player.state='jump_right'
+            elif keys_pressed[pygame.K_a]:
+                player.state='jump_left'
+        if keys_pressed[pygame.K_SPACE]:
+            player.state='throw'
+        if keys_pressed[pygame.K_s]:
+            player.state='pick'
     pygame.display.update()
     clock.tick()
