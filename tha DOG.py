5a9057ed57c489a6feae4943a3f681b4660547fc
TuRtLesz <turtleszac@gmail.com>
2023-06-05 11:40:15 +0530

    fullscreen_corrections

--- tha DOG.py
+++ tha DOG.py
@@ -1,7 +1,10 @@
 import pygame,sys,time,random
-display_window=pygame.display.set_mode((800,450))
-game_window=pygame.Surface((1600,900))
+pygame.init()
+display_size=[pygame.display.Info().current_w,pygame.display.Info().current_h]
+game_window=pygame.Surface((display_size[0],display_size[1]))
+display_window=pygame.display.set_mode((display_size[0]//2,display_size[1]//2))
 pygame.display.set_caption('tha_DOG')
+pygame.display.set_icon(pygame.image.load('Data/icon/DOG.png').convert())
 clock=pygame.time.Clock()
 game_mode='in_game'
 game_settings={'fullscreen':False}
@@ -18,9 +21,9 @@ class player(pygame.sprite.Sprite):
         player.state='idle'
     def update(player,delta_time):
         if player.state=='run_right':
-            player.acceleration.x=20
+            player.acceleration.x=30
         if player.state=='run_left':
-            player.acceleration.x=-20
+            player.acceleration.x=-30
         if player.velocity.x>player.max_velocity:
             player.velocity.x=player.max_velocity
         if player.velocity.x<-player.max_velocity:
@@ -39,11 +42,10 @@ player_sprite_group.add(player())
 while game_mode=='in_game':
     delta_time=time.perf_counter()-prevoius_time
     prevoius_time=time.perf_counter()
+    display_window.fill((255,255,255))
     game_window.fill((255,255,255))
     player_sprite_group.update(delta_time)
     player_sprite_group.draw(game_window)
-    game_window=pygame.transform.smoothscale_by(game_window,0.5)
-    display_window.blit(game_window,(0,0))#screen shake here?
     keys_pressed=pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -52,14 +54,14 @@ while game_mode=='in_game':
         elif event.type==pygame.KEYDOWN:
             if event.key==pygame.K_F11:
                 if game_settings['fullscreen']==True:
-                    display_window=pygame.display.set_mode((800,450))
+                    display_window=pygame.display.set_mode((display_size[0]//2,display_size[1]//2))
                     game_settings['fullscreen']=False
                 elif game_settings['fullscreen']==False:
-                    display_window=pygame.display.set_mode((800,450),pygame.FULLSCREEN|pygame.SCALED)
+                    display_window=pygame.display.set_mode((display_size[0],display_size[1]),pygame.FULLSCREEN|pygame.SCALED)
                     game_settings['fullscreen']=True
             if event.key==pygame.K_ESCAPE:
                 if game_settings['fullscreen']==True:
-                    display_window=pygame.display.set_mode((800,450))
+                    display_window=pygame.display.set_mode((display_size[0]//2,display_size[1]//2))
                     game_settings['fullscreen']=False
     for player in player_sprite_group:
         if keys_pressed[pygame.K_d]:
@@ -76,5 +78,10 @@ while game_mode=='in_game':
             player.state='throw'
         if keys_pressed[pygame.K_s]:
             player.state='pick'
+    game_window.blit(pygame.image.load('rough.png').convert(),(0,450))#testin
+    if game_settings['fullscreen']:
+        display_window.blit(game_window,(0,0))
+    else:
+        display_window.blit(pygame.transform.smoothscale_by(game_window,0.5),(0,0))
     pygame.display.update()
     clock.tick()
